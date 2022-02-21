e50c22057bb42b2c9c568c09ca66c09f1180ede6
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-21 10:20:33 -0300

    Add models category subcategory transaction and update account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_130023) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_131755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,42 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_130023) do
     t.float "initial_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "value"
+    t.bigint "category_id"
+    t.bigint "account_id", null: false
+    t.bigint "subcategory_id"
+    t.string "check_number"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["subcategory_id"], name: "index_transactions_on_subcategory_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +72,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_130023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
+  add_foreign_key "categories", "users"
+  add_foreign_key "subcategories", "categories"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "subcategories"
 end
