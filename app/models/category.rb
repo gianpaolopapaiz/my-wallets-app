6a9bee3b3a15da804a1362b51a5efc61660a4841
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-22 09:54:39 -0300

    Add validations and model methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class Category < ApplicationRecord
+  # Associations
   belongs_to :user
+  has_many :transactions
+  has_many :subcategories, dependent: :destroy
+
+  # Validations
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :description, length: { maximum: 50 }
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Subcategory < ApplicationRecord
+  # Associations
   belongs_to :category
+  has_many :transactions
+  has_one :user, through: :category
+  # Validations
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :description, length: { maximum: 50 }
 end
