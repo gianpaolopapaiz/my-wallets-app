72512591a6eb09240566cb81eb49db0cf4ea2797
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-23 10:26:19 -0300

    Add application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def nice_date(date)
+    date.strftime('%m/%d/%y')
+  end
+
+  def long_date(date)
+    date.strftime('%B %d, %Y')
+  end
 end
