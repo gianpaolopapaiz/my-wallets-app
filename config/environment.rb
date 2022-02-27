fa9a91bd317f4f9aa95d3fba5fbcb4200e31f2aa
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-27 16:36:42 -0300

    Change config mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { host: 'https://my-wallets-app.herokuapp.com' }
