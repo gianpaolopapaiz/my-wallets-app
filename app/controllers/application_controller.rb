1078163d4c1bf301f3ca277c71794db31918a5f2
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-05-22 13:40:03 -0300

    Update rails to 7.0.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
